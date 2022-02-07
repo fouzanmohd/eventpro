7eb7aa9bf00b5501e329a5b9f4abcfa5102eabdc
Muhammed Fouzan <fouzanpharmd@gmail.com>
2022-02-07 13:45:41 +0000

    NavBar updated

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import EventDashboard from './../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World! with a Class</h1>
-      </div>
+      <>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </>
     );
   }
 }
